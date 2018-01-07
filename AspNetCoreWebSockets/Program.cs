c391319b955246dfdfd620c0ebe85041e88087ec
Kimserey Lam <kimserey.lam@groomgy.com>
2018-01-07 10:21:31 +0800

    initial commit

--- AspNetCoreWebSockets/Program.cs
+++ AspNetCoreWebSockets/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace AspNetCoreWebSockets
 {
@@ -20,6 +14,7 @@ namespace AspNetCoreWebSockets
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000")
                 .Build();
     }
 }
